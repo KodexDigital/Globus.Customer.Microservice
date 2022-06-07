2683aeb2707665173bb5e121c26e11579787a2d3
KENNETH OTORO <kenneth.otoro@thegiggroupng.com>
2022-06-07 21:48:28 +0100

    db contection and configuration

---
+++ Globus.DAL/Data/CustomerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globus.DAL.Data
+{
+    public class CustomerDbContext : IdentityDbContext
+    {
+        public CustomerDbContext(DbContextOptions<CustomerDbContext> options) : base(options) { }
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,6 +1,12 @@
+using Globus.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("Dbcon");
+builder.Services.AddDbContextPool<CustomerDbContext>(opt => opt.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
