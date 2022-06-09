28bcaff25aa44bddb768b5b35ee4cdc754058e51
KENNETH OTORO <kenneth.otoro@thegiggroupng.com>
2022-06-09 07:34:07 +0100

    All test cases covevered

---
+++ CustomerOnboardingTests/GoldServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Globus.External.Service.Services;
+using Moq;
+using Xunit;
+
+namespace CustomerOnboardingTests
+{
+    public class GoldServiceTests
+    {
+        [Fact]
+        public void Get_Gold_Live_Price_Test()
+        {
+            var goldService = new Mock<IGoldService>();
+            goldService.Setup(g => g.GetGoldPriceLive())
+                .ReturnsAsync(new Globus.Core.Common.ResponseModel<Globus.External.Service.Responses.GoldLivePriceResponse>());
+        }
+    }
+}
---
+++ CustomerOnboardingTests/UtilitiesTests.cs
@@ -0,0 +1,32 @@
+﻿using Globus.Core.Utils.Hashing;
+using Globus.Core.Utils.Helpers;
+using Xunit;
+
+namespace CustomerOnboardingTests
+{
+    public class UtilitiesTests
+    {
+        [Fact]
+        public void OTP_Helper_Generate_Test()
+        {
+            var otpHelper = OTPHelper.GenerateSimpleOTP();
+            Assert.NotNull(otpHelper);
+            Assert.NotEmpty(otpHelper);
+            Assert.InRange(6, 1, 6);
+        }
+
+        [Fact]
+        public void Password_Hash_Test()
+        {
+            var hash = PasswordHash.EncryptData("070", "1111");
+            Assert.NotEmpty(hash); 
+        }
+
+        [Fact]
+        public void Un_Hash_Password_Test()
+        {
+            var hash = PasswordHash.DecryptData("070", "IwpQnJLP1D7G7ENT8GE8LQ==");
+            Assert.NotEmpty(hash);
+        }
+    }
+}
